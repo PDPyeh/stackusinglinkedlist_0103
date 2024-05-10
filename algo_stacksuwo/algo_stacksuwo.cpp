7c38d1f999c18d3146af58f3cb47ef96a5dd11c2
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-10 09:30:44 +0700

    integer push in class stack

--- algo_stacksuwo/algo_stacksuwo.cpp
+++ algo_stacksuwo/algo_stacksuwo.cpp
@@ -19,5 +19,12 @@ public:
 	Stack() {
 		top = NULL;
 	}
+	int push(int value) {
+		Node* newNode = new Node();
+		newNode->data = value;
+		newNode->next = top;
+		top = newNode;
+		cout << "Push Value : " << value << endl;
+		return value;
 
 
