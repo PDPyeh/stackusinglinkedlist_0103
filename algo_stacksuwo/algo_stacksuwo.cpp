d6debfe0983ec95b8fb06fd2631965f97fe4ea97
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-10 09:31:42 +0700

    conditional statement IF di void pop

--- algo_stacksuwo/algo_stacksuwo.cpp
+++ algo_stacksuwo/algo_stacksuwo.cpp
@@ -28,5 +28,10 @@ public:
 		return value;
 	}
 	void pop() {
-
+		if (isEmpty()) {
+			cout << "Stack is Empty." << endl;
+		}
+		cout << "Popped Value : " << top->data << endl;
+		top = top->next;
+	}
 
